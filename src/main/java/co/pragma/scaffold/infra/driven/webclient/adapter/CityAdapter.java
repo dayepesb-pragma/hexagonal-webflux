4583d8847f6ef6b29d2ecbd2ac0718f1547396cf
david.yepes <david.yepesb@pragma.com.co>
2025-04-23 22:02:55 -0500

    Add resillence4j

--- src/main/java/co/pragma/scaffold/infra/driven/webclient/adapter/CityAdapter.java
+++ src/main/java/co/pragma/scaffold/infra/driven/webclient/adapter/CityAdapter.java
@@ -2,7 +2,12 @@ package co.pragma.scaffold.infra.driven.webclient.adapter;
 
 import co.pragma.scaffold.domain.ports.out.ICityOutPort;
 import co.pragma.scaffold.infra.driven.webclient.dto.CityInfoDto;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,7 +15,10 @@ import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CityAdapter implements ICityOutPort {
+
+    private static final String CITY_SERVICE = "cityService";
 
     @Value("${webclient.ninja.apikey}")
     private String apiKey;
@@ -18,6 +26,10 @@ public class CityAdapter implements ICityOutPort {
     private final WebClient webClient;
 
     @Override
+    @CircuitBreaker(name = CITY_SERVICE, fallbackMethod = "getCityFallback")
+    @Retry(name = CITY_SERVICE)
+    @Bulkhead(name = CITY_SERVICE)
+    @TimeLimiter(name = CITY_SERVICE)
     public Mono<String> getCityInfo(String cityName) {
         return webClient.get()
                 .uri(uriBuilder ->
@@ -31,5 +43,10 @@ public class CityAdapter implements ICityOutPort {
                 .next()
                 .map(cityInfoDto ->
                         String.format("Ciudad: %s - Pais: %s - lat: %f - lon: %f", cityInfoDto.getName(), cityInfoDto.getCountry(), cityInfoDto.getLatitude(), cityInfoDto.getLongitude()));
+    }
+
+    private Mono<String> getCityFallback(String cityName, Exception e) {
+        log.warn("Fallback: active {}", e.getMessage());
+        return Mono.just("no se pudo obtener los datos de la ciudad :(");
     }
 }
--- src/main/java/co/pragma/scaffold/infra/driven/webclient/config/WebClientConfig.java
+++ src/main/java/co/pragma/scaffold/infra/driven/webclient/config/WebClientConfig.java
@@ -1,9 +1,18 @@
 package co.pragma.scaffold.infra.driven.webclient.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 
 @Configuration
 public class WebClientConfig {
@@ -13,7 +22,15 @@ public class WebClientConfig {
 
     @Bean
     public WebClient cityApiClient() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .responseTimeout(Duration.ofMillis(5000))
+                .doOnConnected( conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS))
+                );
         return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .baseUrl(cityBaseUrl)
                 .build();
     }
